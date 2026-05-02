70b97f7f7dbada17252017fa44ce9f3d57861414
Arush Kumar <arushkumarbjn123@gmail.com>
2026-05-02 13:32:07 +0530

    Day1

--- react-refresher/src/main.jsx
+++ react-refresher/src/main.jsx
@@ -1,6 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+// const h1 = React.createElement(
+//   "h1",
+//   {
+//     className: "title",
+//     id: "heading",
+//   },
+//   "Welcome to mobile dev!",
+// );
+
+let name = "App";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <h1 className="title" id="heading">
+      Welcome to Mobile dev! {name}
+    </h1>
+    <App />
+  </>
+);
